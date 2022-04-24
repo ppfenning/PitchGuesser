bbcf13bf297fd5b0b23a8e8a6f949fd54b5b594a
Patrick Pfenning <patrick.pfenning@vtinfo.com>
2022-04-24 19:54:47 -0400

    - fix caching, only search if stored dates are expanded - TODO: split data into test and train - TODO: choose model structure

--- src/PitchGuesser/PitchGuesser.py
+++ src/PitchGuesser/PitchGuesser.py
@@ -12,35 +12,39 @@ bball.cache.enable()
 @dataclass
 class PitchGuesser:
     """Class for modeling pitch types"""
-    start_dt: str = '2021-09-01'
+    start_dt: str = '2021-04-01'
     end_dt: str = dt.now().strftime("%Y-%m-%d")
     refresh: bool = False
 
     def __post_init__(self):
         pdir = Path(__file__).parent.absolute()
-        self.__pkl = f"{os.path.join(pdir, 'data')}/pitch_data.pkl"
-        self.__cols = pd.read_csv(f"{os.path.join(pdir, 'data')}/cols.csv", header=None).squeeze().to_list()
+        datadir = os.path.join(pdir, 'data')
+        self.__pkl = f"{datadir}/pitch_data.pkl"
+        self.__cols = pd.read_csv(f"{datadir}/cols.csv", header=None).squeeze().to_list()
         self.start_ts = dt.strptime(self.start_dt, "%Y-%m-%d")
         self.end_ts = dt.strptime(self.end_dt, "%Y-%m-%d")
         self.data = self.__refresh_data()
 
-    def __get_to_date(self, df):
+    def __get_from_date(self, df):
         if self.start_ts < df.game_date.min():
             df = pd.concat([df, bball.statcast(
-                self.start_dt, dt.strftime(df.game_date.min(), "%Y-%m-%d")
+                start_dt=self.start_dt,
+                end_dt=dt.strftime(df.game_date.min(), "%Y-%m-%d")
             )])
         return df
 
-    def __get_from_date(self, df):
+    def __get_to_date(self, df):
         if self.end_ts > df.game_date.max() + td(days=1):
             df = pd.concat([df, bball.statcast(
-                self.start_dt, dt.strftime(df.game_date.max(), "%Y-%m-%d")
+                start_dt=dt.strftime(df.game_date.max(), "%Y-%m-%d"),
+                end_dt=self.end_dt
             )])
         return df
 
     def __get_end_dates(self, df):
         df = self.__get_from_date(df)
-        return self.__get_to_date(df)
+        df = self.__get_to_date(df)
+        return df.loc[(df['game_date'] >= self.start_ts) & (df['game_date'] <= self.end_ts)]
 
     def __get_cols(self, df):
         return df[self.__cols]
@@ -56,4 +60,4 @@ class PitchGuesser:
         return df
 
 if __name__ == '__main__':
-    test = PitchGuesser(refresh=True)
+    test = PitchGuesser(start_dt='2022-03-01')
