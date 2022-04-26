498d50a108c58d69966c6dbbf06fab41fd63c32e
Patrick Pfenning <patrick.pfenning@vtinfo.com>
2022-04-26 16:53:03 -0400

    - break up data and splitter - init attempt at LogReg - TODO: choose two more models

--- src/PitchGuesser/PitchGuesser.py
+++ src/PitchGuesser/PitchGuesser.py
@@ -4,8 +4,10 @@ from datetime import timedelta as td
 from pathlib import Path
 from sklearn.metrics import accuracy_score, classification_report
 from sklearn.model_selection import train_test_split
-from sklearn.multiclass import OneVsRestClassifier
+import numpy as np
+import scipy.optimize as opt
 from sklearn.preprocessing import LabelEncoder
+from sklearn.metrics import confusion_matrix
 from sklearn.svm import SVC
 import os
 import pandas as pd
@@ -16,7 +18,7 @@ bball.cache.enable()
 
 
 @dataclass
-class PitchGuesser:
+class PitchData:
     """Class for modeling pitch types"""
     start_dt: str = '2022-03-17'  # start day for
     end_dt: str = dt.today().strftime("%Y-%m-%d")
@@ -31,9 +33,6 @@ class PitchGuesser:
         self.__start_ts = dt.strptime(self.start_dt, "%Y-%m-%d")
         self.__end_ts = dt.strptime(self.end_dt, "%Y-%m-%d")
         self.raw_data = self.__get_data()
-        self.__splitter()
-        #self.model = self.__get_model()
-        #self.prediction = self.__predict()
 
     def __get_from_date(self, df):
         if self.__start_ts < df.game_date.min():
@@ -54,39 +53,23 @@ class PitchGuesser:
     def __clean_data(self, df):
         df = df.dropna(subset=['release_speed', 'release_pos_x', 'release_pos_z'])
         df.to_pickle(self.__pkl)
-        df = self.__get_cols(df)
         df = self.__preprocess(df)
+        df = self.__get_cols(df)
         df = df.fillna(method='ffill')
         return df.loc[(df['game_date'] >= self.__start_ts) & (df['game_date'] <= self.__end_ts)]
 
     def __preprocess(self, df):
-        self.class_lst = dict()
-        for cname, ctype in df.dtypes.items():
-            if ctype == 'object':
-                le = LabelEncoder()
-                df[cname] = le.fit_transform(df[cname])
-                self.class_lst[cname] = le
-        return df
-
-    def __splitter(self):
-
-        skip_cols = (
-            'game_date',
-            'pitcher',
-            'player_name',
-            'pitch_type',
-            'pitch_name',
-        )
-
-        X = self.raw_data.loc[:, ~self.raw_data.columns.isin(skip_cols)]
-        y = self.raw_data.pitch_type
-
-        self.X_train, self.X_test, self.y_train, self.y_test = train_test_split(
-            X,
-            y,
-            test_size=0.3,
-            random_state=42
-        )
+        df2 = df.copy()
+        df2['lefty'] = (df.p_throws == 'L').astype(int)
+        df2['righty'] = (df.p_throws == 'R').astype(int)
+        df2['ball'] = (df.type == 'B').astype(int)
+        df2['strike'] = (df.type == 'S').astype(int)
+        df2['hit_in_play'] = (df.type == 'X').astype(int)
+        le = LabelEncoder()
+        df2['pitch_type'] = le.fit_transform(df['pitch_type'])
+        for spin in ['release_spin_rate', 'spin_axis']:
+            df2[spin] = (df[spin] - np.min(df[spin]))/(np.max(df[spin])-np.min(df[spin]))
+        return df2
 
     def __get_data(self):
         if os.path.exists(self.__pkl) and not self.refresh:
@@ -113,6 +96,87 @@ class PitchGuesser:
         print(f"Test Set Accuracy : {accuracy_score(self.y_test, self.prediction) *100} % \n\n")
         print(f"Classification Report : \n\n{classification_report(self.y_test, self.prediction)}")
 
+@dataclass
+class PitchModelBase(PitchData):
+    test_size = 0.30
+
+    def __post_init__(self):
+        super(PitchModelBase, self).__post_init__()
+        self.__splitter()
+
+    def __set_X_y(self):
+
+        skip_cols = (
+            'game_date',
+            'pitcher',
+            'player_name',
+            'pitch_type',
+            'pitch_name',
+            'p_throws',
+            'type',
+        )
+
+        X = self.raw_data.loc[:, ~self.raw_data.columns.isin(skip_cols)].copy()
+        y = self.raw_data.pitch_type
+
+        return X, y
+
+    def __splitter(self):
+
+        X, y = self.__set_X_y()
+
+        self.X_train, self.X_test, self.y_train, self.y_test = train_test_split(
+            X,
+            y,
+            test_size=self.test_size,
+            random_state=42
+        )
+
+
+@dataclass
+class PitchLR(PitchModelBase):
+    
+    def __post_init__(self):
+        super(PitchLR, self).__post_init__()
+        self.X_test = self.__get_interc(self.X_test)
+        self.X_train = self.__get_interc(self.X_train)
+        self.classifiers = self.__getClassifier()
+
+    @staticmethod
+    def __get_interc(df):
+        df2 = df.copy()
+        df2.loc[:, 'inter'] = 1.
+        return df2.loc[:, ['inter'] + list(df.columns)]
+
+    @staticmethod
+    def __sigmoid(z):
+        return 1 / (1 + np.exp(-z.astype(np.float64)))
+
+    def __cost(self, theta, X, y):
+        predictions = self.__sigmoid(X @ theta)
+        predictions[predictions == 1] = 0.999  # log(1)=0 causes error in division
+        error = -y * np.log(predictions) - (1 - y) * np.log(1 - predictions)
+        return sum(error) / len(y)
+
+    def __cost_gradient(self, theta, X, y):
+        predictions = self.__sigmoid(X @ theta)
+        return X.transpose() @ (predictions - y) / len(y)
+
+    def __getClassifier(self):
+        num_labels = len(self.y_test.unique())
+        num_features = self.X_test.shape[1]
+        classifiers = np.zeros(shape=(num_labels, num_features))
+        for c in range(num_labels):
+            label = (self.y_test == c).astype(int)
+            init_theta = np.zeros(num_features)
+            print(c)
+            #classifiers[c, :] = opt.fmin_cg(
+            #    self.__cost,
+            #    init_theta,
+            #    self.__cost_gradient,
+            #    (self.X_test, label),
+            #)
+        return classifiers
 
 if __name__ == '__main__':
-    test = PitchGuesser()
+    test = PitchLR(start_dt='2022-04-20')
