c4c32afd05332396f27de99e44b534ea2d21b331
Patrick Pfenning <patrick.pfenning@vtinfo.com>
2022-04-27 10:39:16 -0400

    - Build and save base model - TODO: visuals and post-processing - TODO: GBC and one more model

--- src/PitchGuesser/PitchGuesser.py
+++ src/PitchGuesser/PitchGuesser.py
@@ -2,19 +2,23 @@ from dataclasses import dataclass
 from datetime import datetime as dt
 from datetime import timedelta as td
 from pathlib import Path
-from sklearn.ensemble import RandomForestClassifier
-from sklearn.metrics import accuracy_score, classification_report
-from sklearn.metrics import confusion_matrix
-from sklearn.model_selection import train_test_split
+from sklearn.ensemble import GradientBoostingClassifier, RandomForestClassifier
+from sklearn.metrics import accuracy_score, classification_report, confusion_matrix
+from sklearn.model_selection import train_test_split, GridSearchCV
 from sklearn.preprocessing import LabelEncoder
+import matplotlib.pyplot as plt
 import numpy as np
 import os
 import pandas as pd
+import pickle
 import pybaseball as bball
+import seaborn as sns
 import tempfile
 
 bball.cache.enable()
-
+tmpdir = f'{tempfile.gettempdir()}/PitchGuesser'
+if not os.path.exists(tmpdir):
+    os.mkdir(tmpdir)
 
 @dataclass
 class PitchData:
@@ -26,11 +30,15 @@ class PitchData:
     def __post_init__(self):
         pdir = Path(__file__).parent.absolute()
         self.datadir = os.path.join(pdir, 'data')
-        tmpdir = tempfile.gettempdir()
         self.__pkl = f"{tmpdir}/pitch_data.pkl"
+        self.pitches = ['4-Seam Fastball', 'Changeup', 'Curveball', 'Cutter', 'Sinker', 'Slider']
         self.__start_ts = dt.strptime(self.start_dt, "%Y-%m-%d")
         self.__end_ts = dt.strptime(self.end_dt, "%Y-%m-%d")
         self.raw_data = self.__get_data()
+        self.features = {
+            'numeric': 1,
+            'categorical': 1
+        }
 
     def __get_from_date(self, df):
         if self.__start_ts < df.game_date.min():
@@ -55,8 +63,7 @@ class PitchData:
         df = self.__get_cols(df)
         df = df.fillna(method='ffill')
         # most common pitches
-        pitches = ['4-Seam Fastball', 'Changeup', 'Curveball', 'Cutter', 'Sinker', 'Slider']
-        df = df.loc[df.pitch_name.isin(pitches), :].sort_values(by=['pitch_name', 'game_date', 'pitcher'])
+        df = df.loc[df.pitch_name.isin(self.pitches), :].sort_values(by=['pitch_name', 'game_date', 'pitcher'])
         return df.loc[(df['game_date'] >= self.__start_ts) & (df['game_date'] <= self.__end_ts), :]
 
     def __scaler(self, df):
@@ -68,11 +75,11 @@ class PitchData:
 
     def __preprocess(self, df):
         df2 = df.copy()
-        df2['lefty'] = (df.p_throws == 'L').astype(int)
-        df2['righty'] = (df.p_throws == 'R').astype(int)
-        df2['ball'] = (df.type == 'B').astype(int)
-        df2['strike'] = (df.type == 'S').astype(int)
-        df2['hit_in_play'] = (df.type == 'X').astype(int)
+        df2['lefty'] = df.p_throws == 'L'
+        df2['righty'] = df.p_throws == 'R'
+        df2['ball'] = df.type == 'B'
+        df2['strike'] = df.type == 'S'
+        df2['hit_in_play'] = df.type == 'X'
         df2 = self.__scaler(df2)
         return df2
 
@@ -90,18 +97,20 @@ class PitchData:
         return df[cols]
 
 @dataclass
-class PitchModelBase(PitchData):
+class PitchModelBuild(PitchData):
     model = None
+    model_pkl = None
     test_size: float = 0.30
+    random_state = 42
 
     def __post_init__(self):
         if self.model is None:
             raise ValueError('A model must be passed')
-        super(PitchModelBase, self).__post_init__()
+        if self.model_pkl is None:
+            raise ValueError('A model pickle file must be passed')
+        super(PitchModelBuild, self).__post_init__()
         self.__splitter()
         self.__fit()
-        self.y_predict = self.__prediction()
-        self.cm = confusion_matrix(self.y_test, self.y_predict)
 
     def __set_X_y(self):
 
@@ -129,24 +138,63 @@ class PitchModelBase(PitchData):
             X,
             y,
             test_size=self.test_size,
-            random_state=42
+            random_state=self.random_state
         )
 
     def __fit(self):
-        self.model.fit(self.X_train, self.y_train)
+        if not os.path.exists(self.model_pkl) or self.refresh:
+            self.model.fit(self.X_train, self.y_train)
+            with open(self.model_pkl, 'wb') as fout:
+                pickle.dump(self.model, fout)
+        else:
+            with open(self.model_pkl, 'rb') as fin:
+                self.model = pickle.load(fin)
+
+@dataclass
+class PitchGuessPost(PitchModelBuild):
+    
+    def __post_init__(self):
+        super(PitchGuessPost, self).__post_init__()
+        self.y_predict = self.__prediction()
+        self.cm = self.__get_cm()
+
+    @property
+    def __feature_types(self):
+        categorical = list(self.X_test.select_dtypes(include=['bool']).columns) + ['zone']
+        numeric = list(self.X_test[~self.X_test.columns.isin(categorical)].columns)
+        return {
+            'categorical': categorical,
+            'numeric': numeric,
+        }
 
     def __prediction(self):
         return self.model.predict(self.X_test)
+
+    def __get_cm(self):
+        return pd.DataFrame(
+            confusion_matrix(self.y_test, self.y_predict),
+            index=self.pitches,
+            columns=self.pitches
+        )
 
     def score(self):
         return accuracy_score(self.y_test, self.y_predict)
 
     def class_report(self):
-        return classification_report(self.y_test, self.y_predict)
+        print(classification_report(self.y_test, self.y_predict))
 
 @dataclass
-class PitchRFC(PitchModelBase):
+class PitchRFC(PitchGuessPost):
+    #model = GridSearchCV(
+    #    RandomForestClassifier(n_estimators=100),
+    #    params,
+    #    cv=3,
+    #    n_jobs=-1,
+    #    verbose=1,
+    #    scoring='f1_micro'
+    #)
     model = RandomForestClassifier(n_estimators=100)
+    model_pkl = f'{tmpdir}/RFC.pkl'
 
 if __name__ == '__main__':
     test = PitchRFC(start_dt='2022-03-17')
